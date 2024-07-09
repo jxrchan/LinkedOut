d19244e8c6082595fb7b3045eb13c6ad311c34ff
Hao Li <pedrohaolee@gmail.com>
2024-07-09 13:22:07 +0800

    integrated

---
+++ integrated/Backend/src/models/Employers.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const EmployerSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, minLength: 1, maxLength: 50 },
+    description: { type: String, required: true, maxLength: 2000 },
+    email: { type: String, minLength: 1, maxLength: 50 },
+    jobs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Jobs" }],
+  },
+  { collection: "employers" }
+);
+
+module.exports = mongoose.model("Employers", EmployerSchema);
---
+++ integrated/Frontend/src/components/JobSeekerDashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const JobSeekerDashboard = () => {
+  return (
+    <div>
+      <h1>Welcome Job Seeker!</h1>
+    </div>
+  );
+};
+
+export default JobSeekerDashboard;
