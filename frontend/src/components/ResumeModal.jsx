820b7cdc142537586ae94771a2b6c2403af68a09
Hao Li <pedrohaolee@gmail.com>
2024-07-10 15:09:52 +0800

    Formate resume data

--- frontend/src/components/ResumeModal.jsx
+++ frontend/src/components/ResumeModal.jsx
@@ -2,10 +2,9 @@ import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import useFetch from "../hooks/useFetch";
 import ReactDOM from "react-dom";
-import styles from "./ResumeModal.module.css"
+import styles from "./ResumeModal.module.css";
 
 const OverLay = (props) => {
-
   const usingFetch = useFetch();
 
   const { isSuccess, isError, error, isFetching, data } = useQuery({
@@ -18,31 +17,43 @@ const OverLay = (props) => {
       ),
   });
 
+  const formatResumeData = (data) => {
+    if (!data) return null;
+
+    return (
+      <div className={styles.resumeContent}>
+        {data.split("\n").map((line, index) => (
+          <p key={index} className={styles.resumeLine}>
+            {line}
+          </p>
+        ))}
+      </div>
+    );
+  };
+
   return (
     <div className={styles.backdrop}>
-    <div className={styles.modal}>
+      <div className={styles.modal}>
+        {isFetching && <h1>Loading...</h1>}
+        {isError && <div>{error.message}</div>}
+        {console.log(data)}
+        {isSuccess && formatResumeData(data)}
 
-    {isFetching && <h1>Loading...</h1>}
-    {isError && <div>{error.message}</div>}
-    {console.log(data)}
-    {isSuccess && data && data}
-    
-      <br />
-      <div className="row">
-        <div className="col-md-3"></div>
-        <button
-          className="col-md-6"
-          onClick={() => props.setShowResumeModal(false)}
-        >
-          Cancel
-        </button>
-        <div className="col-md-3"></div>
+        <br />
+        <div className="row">
+          <div className="col-md-3"></div>
+          <button
+            className="col-md-6"
+            onClick={() => props.setShowResumeModal(false)}
+          >
+            Cancel
+          </button>
+          <div className="col-md-3"></div>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
 };
-
 
 const ResumeUpdateModal = (props) => {
   return (
@@ -60,4 +71,3 @@ const ResumeUpdateModal = (props) => {
 };
 
 export default ResumeUpdateModal;
-
