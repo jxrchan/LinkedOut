358c4557702affe6e0a85d94a53715a6d223f027
pedrohaolee <168340854+pedrohaolee@users.noreply.github.com>
2024-07-10 16:44:49 +0800

    Delete integrated-working directory

--- integrated-working/Backend/src/routers/fileUpload.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { uploadPdfFile } = require("../controllers/fileUpload");
-const { checkErrors } = require("../validators/checkErrors");
-const { validateApplyJobData } = require("../validators/applicants");
-
-router.post("/pdf", uploadPdfFile);
-
-module.exports = router;
