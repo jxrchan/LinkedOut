da04583aa968d8c877a64ce010976aaf9322a1c3
pedrohaolee <168340854+pedrohaolee@users.noreply.github.com>
2024-07-10 16:44:32 +0800

    Delete integrated - Copy directory

--- integrated - Copy/Backend/src/routers/roles.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getAllRoles } = require("../controllers/roles");
-
-router.get("/", getAllRoles);
-
-module.exports = router;
